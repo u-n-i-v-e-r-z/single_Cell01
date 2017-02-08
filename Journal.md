efec873341eb7428d53f44bc927f30ab1fb58ab3
alexheurteau <a.heurteau@gmail.com>
2017-02-08 13:10:45 +0100

    Droso dm6 rds

---
+++ Journal.md
@@ -0,0 +1,18 @@
+# CUVIER'S TEAM
+# Heurteau
+# SINGLE CELL PROJECT
+
+# Configuration : https://github.com/u-n-i-v-e-r-z/single_Cell01 (Clone with ssh if on Genotoul)
+
+The aim of this study is to analyze the effect of Mes4 depletion (siRNA) on Heterochromatin spreading and its effect upon genes expression.
+
+
+# 02/02/17
+Among the 19 single celle exp, all have been contaminated resulting in so few reads that will be used (max 40% of Uniquely mapped)
+Starting to play with RNAseq script on Rstudio localy.
+
+
+
+
+#03/02/17
+We're doing the Alignement of Drosophila on dm6 genome with STAR (see STAR_alignment_noGFF.R)
--- exp/3_RNAseq_analysis/bin/rnaSeqGene_workflow.R
+++ exp/3_RNAseq_analysis/bin/rnaSeqGene_workflow.R
@@ -27,40 +27,71 @@ library("Rsamtools")
 #          SET PATH & LOAD DATA ------------
 #
 #####################################################################################
-TxDb.Dmelanogaster.UCSC.dm6.ensGene
+#DM6
+txdb <- TxDb.Dmelanogaster.UCSC.dm6.ensGene
+dir <- "/media/alexandre/Data/Recherche/LBME/Projects/sCell_RNAseq_clone/exp/2_Alignment/results/str_dmel613_sted_bam/"
+xp_list <- unlist(lapply(read.csv("/media/alexandre/Data/Recherche/LBME/Projects/sCell_RNAseq_clone/exp/2_Alignment/results/str_dmel613_sted_bam/exp_list.csv",h=F,sep="\n"),as.character))
+filenames <- file.path(dir,paste0(xp_list))
+file.exists(filenames)
+bamfiles <- BamFileList(filenames, yieldSize=2000000)
+#seqinfo(bamfiles[1])
+xp_type <- readRDS("/media/alexandre/Data/Recherche/LBME/Projects/sCell_RNAseq_clone/exp/3_RNAseq_analysis/data/sampleTable.rds")
+
+
+txdb <- TxDb.Dmelanogaster.UCSC.dm3.ensGene
+ebg <- genes(txdb)
+seqlevelsStyle(ebg) <- "Ensembl"
+#Remove the ".1" suffixe on dm6 gene IDs
+names(ebg) <- gsub("[.]1$","",names(ebg))
+elementMetadata(ebg)$gene_id <- names(ebg)
+#Create assay of #reads/gene
+se_dm6 <- summarizeOverlaps(features=ebg, reads=bamfiles, mode="Union", singleEnd=FALSE, ignore.strand=TRUE, fragments=TRUE)
+
+
+
+#DM3
+txdb <- TxDb.Dmelanogaster.UCSC.dm3.ensGene
 dir <- "/media/alexandre/Data/Recherche/LBME/Projects/sCell_RNAseq_clone/exp/2_Alignment/results/bam_files/star_alignment_sorted/"
 xp_list <- unlist(lapply(read.csv("/media/alexandre/Data/Recherche/LBME/Projects/sCell_RNAseq_clone/exp/2_Alignment/results/bam_files/star_alignment_sorted/exp_list.csv",h=F,sep="\n"),as.character))
 filenames <- file.path(dir,paste0(xp_list))
 file.exists(filenames)
-
 bamfiles <- BamFileList(filenames, yieldSize=2000000)
-seqinfo(bamfiles[1])
+#seqinfo(bamfiles[1])
+xp_type <- readRDS("/media/alexandre/Data/Recherche/LBME/Projects/sCell_RNAseq_clone/exp/3_RNAseq_analysis/data/sampleTable.rds")
+ebg <- genes(txdb)
+seqlevelsStyle(ebg) <- "Ensembl"
+#Create assay of #reads/gene
+se_dm3 <- summarizeOverlaps(features=ebg, reads=bamfiles, mode="Union", singleEnd=FALSE, ignore.strand=TRUE, fragments=TRUE)
+
+
+
+# ACTIVE GENES LISTS
+ac_genes.vec <- readRDS("/media/alexandre/Data/Recherche/LBME/Projects/sCell_RNAseq_clone/raw/Lists/Active_genes/xpaus.rds")
+
 #####################################################################################
 #
 #          TREATMENT ------------
 #
 #####################################################################################
-txdb <- TxDb.Dmelanogaster.UCSC.dm3.ensGene
-ebg <- exonsBy(txdb, by="gene")
+count_matrix_dm3 <- assay(se_dm3)
+count_matrix_dm6 <- assay(se_dm6)
 
-se <- summarizeOverlaps(features=ebg, reads=bamfiles,
-                        mode="Union",
-                        singleEnd=FALSE,
-                        ignore.strand=TRUE,
-                        fragments=TRUE)
+sum(colSums(count_matrix_dm6))
+sum(colSums(count_matrix_dm3))
 
+plot(colSums(count_matrix_dm6),colSums(count_matrix_dm3))
+abline(a=1,b=1)
 
-count_matrix <- assay(se)
+apply(count_matrix>0,2,sum)
+
 
 # DO SOME PLOTS TO VISUALIZE DATA
 nb_reads_xp <- colSums(count_matrix)
-plot(nb_reads_xp,apply(count_matrix>0,2,sum))
+plot(nb_reads_xp,apply(count_matrix>0,2,sum),xlim=c(0,300000))
+
+dm6_nb_gene <- nb_reads_xp
 # KEEP ONLY GOOD BAM
 count_matrix_sel=count_matrix[,apply(count_matrix>0,2,sum)>1000]
-
-
-
-
 
 which(rowSums(assay(se)) == min(rowSums(assay(se))))
 
